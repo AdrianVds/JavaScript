e540fb9d6c843b23598f57d45ec43beea0465aca
AdrianVds <adrianvds03032001@gmail.com>
2021-09-02 20:28:58 -0300

    aula

--- aula13.js
+++ aula13.js
@@ -1,13 +1,20 @@
 const prompt = require('prompt-sync')();
-var objetoscriados = []
-var rodadasQuantidade = +prompt("quantas rodadas deseja jogar: ")
-for(let i=0;i<rodadasQuantidade;i+=1){
-    let obj = {};
-    // for no for exmplo
-    var qntdjgdor = +prompt("quantos vamo jogar: ")
-      for(let i=0;i<qntdjgdor;i+=1)
-        obj.dados = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
-        obj.nomesJogadores = prompt("nome de jogadores: ")
-        objetoscriados.push(obj)  
-}
-console.log(objetoscriados)   
+let rodadas = parseInt(prompt("quantas rodadas: "))   
+for (let c = 0; c < rodadas; c++) {
+    let jogos = [];
+    let qtsJogadores = parseInt(prompt("quantos jogadores: "))
+    for (let i = 0; i < qtsJogadores; i++) {
+        let jogo = {
+            nome: "Jogador" + (i + 1),
+            numero: Math.floor(Math.random() * 6 + 1),
+        };
+        jogos.push(jogo);
+
+        jogos.sort((a, b) => {
+            if (b.numero < a.numero) {
+                return -1;
+            } else {
+                return true;
+            }
+        });
+    }
