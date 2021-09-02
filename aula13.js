7272342176cc60ef504c7fe73a6b25dbb34ff055
AdrianVds <adrianvds03032001@gmail.com>
2021-09-01 23:00:26 -0300

    comitinho

--- aula13.js
+++ aula13.js
@@ -3,9 +3,11 @@ var objetoscriados = []
 var rodadasQuantidade = +prompt("quantas rodadas deseja jogar: ")
 for(let i=0;i<rodadasQuantidade;i+=1){
     let obj = {};
-    // for no for
-    obj.dados = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
-    obj.nomesJogadores = prompt("nome de jogadores: ")
-    objetoscriados.push(obj)  
+    // for no for exmplo
+    var qntdjgdor = +prompt("quantos vamo jogar: ")
+      for(let i=0;i<qntdjgdor;i+=1)
+        obj.dados = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
+        obj.nomesJogadores = prompt("nome de jogadores: ")
+        objetoscriados.push(obj)  
 }
 console.log(objetoscriados)   
