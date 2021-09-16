d51d3c0d0c096d2025ae8ad33b87a2d6b2297e47
AdrianVds <adrianvds03032001@gmail.com>
2021-09-15 23:31:00 -0300

    projeto final -campeonato

--- pjto-final.js
+++ pjto-final.js
@@ -1,55 +1,86 @@
 const prompt = require('prompt-sync')();({sigint: true});
-// Final de Campeonato em 7 dias, o pivo ${jogador} precisa treinar pesado, pelo menos "5 dias", para ter 
-//condicionalmento sufiente para seu time vencer !!
 var dias_treinados = 0;
 var dias_para_final = 7;
-console.log()
-console.log("------------------------INICIANDO O JOGO-------------------------------------")
-console.log("...Jogadores, o treino intensivo começa amanhã.. Voces precisam chegar as 8:00")
-console.log("Não quero ninguem chegando atrasado e nem sem comer, caso aconteça hávera punição!")
-console.log("----------------------------------------------------------------------------------")
-console.log("voce precisa treinar no minimo 5 dias treinado para vencer o campeonato! ")
-console.log("----------------------------------------------------------------------------------")
-console.log("Primeiro dia, opções: digite (1) para  ir treinar ou Digite (2) para aproveitar o dia todo ")
-class Jogador{
+console.log("----------------------------------------------------------------------------");
+console.log("Efetue seu Login:");
+let nome_jogador = prompt("Nome do jogador: ");
+let idade_jogador = +prompt("idade do jogador: ");
+console.log();
+console.log("------------------------INICIANDO O JOGO-------------------------------------");
+console.log("Treinador: ...Jogadores, o treino intensivo começa amanhã, Voces precisam chegar as 8:00 da manhã");
+console.log("Não quero ninguem chegando atrasado e nem sem comer, caso aconteça hávera punição!");
+console.log("----------------------------------------------------------------------------------");
+console.log("OBJETIVO: voce precisa ter no minimo 5 dias treinados para vencer o campeonato! ");
+console.log("DIFICULDADE: voce tem apenas 7 dias !");
+console.log("----------------------------------------------------------------------------------");
+console.log("Primeiro dia, opções: digite (1) para  ir treinar ou Digite (2) para aproveitar o dia");
+console.log();
+class Treinamento{
     constructor(nome){
         this.nome = nome;    
     }
     funcao_dias(){
         while(dias_para_final >= 4){
-            var dia = +prompt(`${this.nome} Qual opçaõ deseja: `)
+            var dia = +prompt(`${this.nome} Qual opçaõ deseja: `);
             if (dia === 1){
                 dias_treinados +=1;
                 dias_para_final -=1;
-                console.log("Voce treinou muito, descance bem")
-            }
-            else if (dia === 2){
+                console.log("Voce treinou muito hoje, descance bem para amanhã...");
+                console.log();
+            }else if (dia === 2){
                 dias_para_final -=2;
-                console.log("Voce se divertiu demais, perdeu 2 dia de treino!")
-            }
+                console.log("Voce se divertiu de mais, e teve que ficar um dia na reabilitação, perdeu 2 dia de treino!...")
+                console.log();
+            }else{
+                console.log("Houve um Erro, digite apenas 1 ou 2 !!");
+            };
+        for(let i = 3;dias_para_final <= i;){
+            console.log("----------------------------------------------------------------------------------");
+            console.log(`ja se foi mais de ${dias_para_final} dias e voce so treinou ${dias_treinados}! `);
+            console.log();
+            console.log("...Imprevisto!!!, seu despertador nâo tocou e voce acordou tarde!!");
+            console.log("Escolha com Sabedoria seus proximos passos para que tudo ocorra bem");
+            console.log();
+            console.log("1-para: comer e chegar atrado, 2-para: chegar na hora sem comer,");
+            console.log("----------------------------------------------------------------------------------");
+            break
         };
-        while(dias_para_final <=3 && dias_para_final >= 0){
-            var desafio = +prompt(`${this.nome} Qual opção deseja1: `)
+        };
+        while(dias_para_final <= 3 && dias_para_final >= 1){
+            var desafio = +prompt(`${this.nome} Escolha rapido! voce não tem tempo: `);
             if(desafio === 1){
                 dias_para_final -=1;
                 dias_treinados +=1;
-                console.log("Seu treinador esta de olho em voce")
+                console.log("Seu treinador esta de olho em voce, cuidado");
             }else if ( desafio ===2){
-                dias_para_final -=2
-                console.log("Voce não comeu e passou mal, perdeu 2 dias na recuperação")
-
-            }
-
-        }
-        for(let i = 3;dias_para_final <= i;){
-            console.log("...Imprevisto!!!, seu despertador nâo tocou e voce acordou tarde!!")
-            console.log("Escolha com Sabedoria seus proximos passos para que tudo ocorra bem")
-            console.log("1-para: comer e chegar atrado, 2-para: chegar na hora sem comer,")
+                dias_para_final -=2;
+                console.log("Voce não comeu e passou mal, perdeu 2 dias na recuperação");
+            }else{
+                console.log("Houve um Erro, digite apenas 1 ou 2 !!");
+            };
         };
+        console.log("------------Final-de-Jogo--------------------");
     };
 }
-let nome_jogador = prompt("Qual o seu nome jogador: ")
-let nome1 = new Jogador(nome_jogador)
+class Resultado extends Treinamento {
+    constructor(nome,idade){
+        super(nome);
+        this.idade = idade;
+    }
+    resultados(){
+        if(dias_treinados >= 5){
+            console.log("RESULTADO...");
+            console.log(`Parabens,Voce Treinou ${dias_treinados} dias Intensos e conseguiu vencer o campeonato Sub: ${idade_jogador} do LEC!!!!`)
+            console.log();
+        }else{
+            console.log("RESULTADO...");
+            console.log(`Não foi desta vez ${nome_jogador}, Voce treinou apenas ${dias_treinados} e nao foi o suficiente para vencer !!`)
+            console.log();
+        };
+    };
+};
+
+let nome1 = new Treinamento(nome_jogador);
+let idade = new Resultado(nome_jogador,idade_jogador);
 nome1.funcao_dias();
-///// talvez chamar o for na herança ver oque podemos fazer
-//// ou fazer a final na sub classe com o resultado ////
+idade.resultados();
